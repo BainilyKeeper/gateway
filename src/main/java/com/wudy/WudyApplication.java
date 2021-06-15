0b7608743caaf8eafea648edbdab625ad7af0ce2
wudy <wudeyuan28931@163.com>
2021-06-15 15:35:57 +0800

    修改依赖

--- src/main/java/com/wudy/WudyApplication.java
+++ src/main/java/com/wudy/WudyApplication.java
@@ -4,12 +4,14 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.wudy"})
 @EnableZuulProxy
 @EnableSwagger2Doc
 @EnableFeignClients(basePackages = {"com.wudy"})
+@EnableEurekaClient
 public class WudyApplication {
 
     public static void main(String[] args) {
